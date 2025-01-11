372e72f02b684e3c5884702d5ff8e48e0adc5ef3
Kenta Kato <kentaktwo@kk2024-m4-mbp.local>
2025-01-12 07:48:25 +0900

    add cmd to list diagnostics

--- mac/dot.config/nvim/lua/plugins/telescope.lua
+++ mac/dot.config/nvim/lua/plugins/telescope.lua
@@ -3,9 +3,10 @@ return {
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { mode = "n", "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Telescope find files" },
-    { mode = "n", "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Telescope live grep" },
-    { mode = "n", "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Telescope buffers" },
-    { mode = "n", "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Telescope help tags" },
+    { mode = "n", "<leader>ff", "<cmd>Telescope find_files<CR>",  desc = "Telescope find files" },
+    { mode = "n", "<leader>fg", "<cmd>Telescope live_grep<CR>",   desc = "Telescope live grep" },
+    { mode = "n", "<leader>fb", "<cmd>Telescope buffers<CR>",     desc = "Telescope buffers" },
+    { mode = "n", "<leader>fh", "<cmd>Telescope help_tags<CR>",   desc = "Telescope help tags" },
+    { mode = "n", "<leader>fd", "<cmd>Telescope diagnostics<CR>", desc = "Telescope diagnostics" },
   },
 }
