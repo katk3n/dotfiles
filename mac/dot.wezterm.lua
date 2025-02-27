600a92a7a7df1385950771f1bbc223ee5cbb3c97
katk3n <kentaktwo@yahoo.com>
2025-02-28 00:50:31 +0900

    use yazi for explorer

--- mac/dot.wezterm.lua
+++ mac/dot.wezterm.lua
@@ -11,4 +11,68 @@ config.window_background_opacity = 0.90
 config.font = wezterm.font("HackGen35 Console NF", { weight = "Medium", stretch = "Normal", style = "Normal" })
 config.font_size = 14
 
+config.leader = {
+  key = "q",
+  mods = "CTRL",
+  timeout_milliseconds = 2000,
+}
+
+config.keys = {
+  {
+    mods = "LEADER",
+    key = "s",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "LEADER",
+    key = "v",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "LEADER",
+    key = "p",
+    action = wezterm.action.PaneSelect,
+  },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = wezterm.action.ActivatePaneDirection "Left",
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = wezterm.action.ActivatePaneDirection "Down",
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = wezterm.action.ActivatePaneDirection "Up",
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = wezterm.action.ActivatePaneDirection "Right",
+  },
+  {
+    mods = "CMD|CTRL|SHIFT",
+    key = "h",
+    action = wezterm.action.AdjustPaneSize { "Left", 2 },
+  },
+  {
+    mods = "CMD|CTRL|SHIFT",
+    key = "j",
+    action = wezterm.action.AdjustPaneSize { "Down", 2 },
+  },
+  {
+    mods = "CMD|CTRL|SHIFT",
+    key = "k",
+    action = wezterm.action.AdjustPaneSize { "Up", 2 },
+  },
+  {
+    mods = "CMD|CTRL|SHIFT",
+    key = "l",
+    action = wezterm.action.AdjustPaneSize { "Right", 2 },
+  },
+}
+
 return config
