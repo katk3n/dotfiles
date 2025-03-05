133363d0bdff44141154d1c290fb5207b5b32dcc
katk3n <kentaktwo@yahoo.com>
2025-03-05 12:59:33 +0900

    use monaspace font

--- mac/dot.wezterm.lua
+++ mac/dot.wezterm.lua
@@ -8,7 +8,11 @@ end
 config.color_scheme = "tokyonight"
 config.window_background_opacity = 0.90
 --config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Medium", stretch = "Normal", style = "Normal" })
-config.font = wezterm.font("HackGen35 Console NF", { weight = "Medium", stretch = "Normal", style = "Normal" })
+--config.font = wezterm.font("HackGen35 Console NF", { weight = "Medium", stretch = "Normal", style = "Normal" })
+config.font = wezterm.font_with_fallback({
+  { family = "Monaspace Argon",       weight = "Medium", stretch = "Normal", style = "Normal" },
+  { family = "Moralerspace Argon NF", weight = "Medium", stretch = "Normal", style = "Normal" },
+})
 config.font_size = 14
 
 config.leader = {
